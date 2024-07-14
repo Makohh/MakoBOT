0d55625bc36e0bada218a69debf9ef9aaf781471
Makohh <mibvg99@gmail.com>
2024-07-14 16:12:33 -0500

    Boost and Aiming
    Updated logic for hitting the ball and grabbing boost

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from util.objects import *
 from util.routines import *
+from util.tools import find_hits
 
 
 class Bot(BotCommandAgent):
@@ -12,6 +13,12 @@ class Bot(BotCommandAgent):
         near_net = abs(self.ball.location.y - self.friend_goal.location.y)
         defending = abs(self.me.location.y - self.friend_goal.location.y)
         is_in_front_of_ball = d1 > d2
+        targets = {
+            'at_opponent_goal': (self.foe_goal.left_post, self.foe_goal.right_post),
+            'away_from_my_goal': (self.friend_goal.right_post, self.friend_goal.left_post)
+        }
+        hits = find_hits(self,targets)
+
         if (self.intent is not None):
             return
         if self.kickoff_flag:
@@ -23,12 +30,34 @@ class Bot(BotCommandAgent):
             print('retreating')
             return
         # if the ball is near our net, return to net and clear
-        if near_net <= abs(1200):
+        if near_net <= abs(1500):
             self.set_intent(goto(self.friend_goal.location))
             print('ball near net')
             if defending < abs(100):
-                self.set_intent(short_shot(self.foe_goal.location))
-                print('defending')
+                if len(hits['away_from_my_goal']) > 0:
+                    self.set_intent(hits['away_from_my_goal'][0])
+                    print('defending')
+                    return
                 return
             return
-        self.set_intent(short_shot(self.foe_goal.location))
+        if len(hits['at_opponent_goal']) > 0:
+            self.set_intent(hits['at_opponent_goal'][0])
+            print('shooting at goal')
+            return
+
+        # Boost Logic
+        available_boosts = [boost for boost in self.boosts if boost.active]
+
+        closest_boost = None
+        closest_distance = 10000
+
+        for boost in available_boosts:
+            distance = (self.me.location - boost.location).magnitude()
+            if closest_boost is None or distance < closest_distance:
+                closest_boost = boost
+                closest_distance = distance
+
+        if closest_boost is not None:
+            self.set_intent(goto(closest_boost.location))
+            print('Grabbing Boost')
+            return
